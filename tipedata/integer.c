eb7af2d007e2b840ea101f119ebc0095a110a859
axel <nshxel@gmail.com>
2025-05-11 21:06:48 +0700

    first commit

---
+++ tipedata/integer.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+  int bil_bulat = 9;
+  // cara cetak nilai bil_bulat
+  printf("%d", bil_bulat);
+
+  // sekedar menampilkan ukuran tipe data
+    printf("Ukuran int: %lu bytes\n", sizeof(bil_bulat));
+  return 0;
+}
