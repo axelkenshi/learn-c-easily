883ebc714adc2d885798d4790bb140f9e30f5e41
axel <nshxel@gmail.com>
2025-05-17 16:21:11 +0700

    strukturprogram

---
+++ struktur_program.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+  // kode yang akan di eksekusi harus di tulis di dalam fungsi main
+  // fungsi main adalah fungsi utama yang akan di eksekusi pertama kali
+
+  // jika tidak ada fungsi main maka program tidak dapat di eksekusi
+  // fungsi main harus di tulis dengan tipe data int
+
+  /*
+  fungsi main harus diakhiri dengan tanda kurung buka dan kurung tutup,
+  begitupula dengan kurung kurawalnya. (berpasangan)
+  */
+
+  // misalnya kita ingin menampilkan tulisan "Hello World" di layar menggunakan sebuah perintah
+  // dalam bahasa pemrograman C, setiap perintah diakhiri dengan tanda titik koma (;)
+  printf("Hello World");
+
+  return 0; // mengembalikan nilai 0 ke sistem operasi
+  // menandakan bahwa program telah selesai di eksekusi
+}
